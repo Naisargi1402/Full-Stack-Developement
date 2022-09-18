b3978cb86b9cbdeb603dd3c615e620ecaeb16008
Naisargi Vadodariya <20cs100@charusat.edu.in>
2022-09-18 19:39:25 +0530

    Practical 6

---
+++ Practical_6/prac6/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import TodoList from './components/TodoList';
+
+function App() {
+  return (
+    <div className='todo-app'>
+      <TodoList />
+      
+    <h1>20CS100_Naisargi Vadodariya</h1>
+    </div>
+  );
+}
+
+export default App;
